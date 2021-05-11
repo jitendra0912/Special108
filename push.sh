a66bdb3051914a82aba4986b13ab11b05035bf2d
Jitendra Agarwal <jitendra.agarwal@capgemini.com>
2021-05-11 23:43:21 +0530

    2021-05-11 23:43:21

--- push.sh
+++ push.sh
@@ -19,5 +19,5 @@ echo "Added the commit with message: '$message'"
 
 # get current branch and push
 current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-git push origin "$current_branch"
+git push -u origin "$current_branch"
 echo "Push changes to '$current_branch' branch"
